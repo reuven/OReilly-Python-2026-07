6dab57a8e4128084c8a09992defd2eaa19c1b678
Reuven Lerner <reuven@lerner.co.il>
2026-08-03 21:23:23 +0300

    gitautosync update 75

--- mymod.py
+++ mymod.py
@@ -1,4 +1,5 @@
-print(f'Hello, and welcome to {__name__}!')
+if __name__ == '__main__':
+    print(f'Hello, and welcome to {__name__}!')   # only print when run directly, not when imported
 
 x = 100
 
@@ -7,4 +8,5 @@ y = [10, 20, 30]
 def hello(name):
     return f'Hello, {name}, from mymod!'
 
-print(f'Goodbye -- thanks for visiting {__name__}!')
+if __name__ == '__main__':
+    print(f'Goodbye -- thanks for visiting {__name__}!')
